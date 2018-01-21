38084703f825974a5d069585ac2fb9751189fa40
Felipe Vieira da Rocha <felipevieiradarocha@hotmail.com>
2018-01-21 19:54:30 -0200

    Commit inicial do projeto

---
+++ ProjetoRpgTeste/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <time.h>
+#include "toolbox.h"
+#include "personagens.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    srand(time(NULL));
+
+    novo_jogo();
+
+    return 0;
+}
+
---
+++ ProjetoRpgTeste/personagens.h
@@ -0,0 +1,25 @@
+#ifndef PERSONAGENS_H_INCLUDED
+#define PERSONAGENS_H_INCLUDED
+
+typedef struct {
+    char nome[20];
+    int habilidade;
+    int energia;
+    int sorte;
+} Personagem;
+
+typedef struct {
+    char nome[20];
+    int habilidade;
+    int energia;
+} Criatura;
+
+typedef struct {
+    char texto[1000];
+    int concluido; /* Se esse desafio foi concluido 1, caso contrário 0 */
+    Criatura criatura;
+    int resposta;
+    int tipo; /* se for desafio 1, se for batalha 2*/
+} Desafio;
+
+#endif // PERSONAGENS_H_INCLUDED
